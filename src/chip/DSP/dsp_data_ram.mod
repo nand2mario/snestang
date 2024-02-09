ea492ac0d63c34b1a65fb645d4a87bdb9bcd8fc2
nand2mario <nand2mario@outlook.com>
2024-02-09 16:19:03 +0800

    add dsp data ram

---
+++ src/chip/DSP/dsp_data_ram.mod
@@ -0,0 +1,24 @@
+-series GW5A
+-device GW5A-25
+-device_version A
+-package MBGA121N
+-part_number GW5A-LV25MG121NC1/I0
+
+
+-mod_name dsp_data_ram
+-file_name dsp_data_ram
+-path D:/Gowin/dev/snestang/src/chip/dsp/
+-type RAM_DP
+-file_type vlg
+-bram_b true
+-dev_type GW5A-25A
+-depth_0 2048
+-depth_1 2048
+-width_0 16
+-width_1 16
+-read_mode_0 bypass
+-read_mode_1 bypass
+-write_mode_0 normal
+-write_mode_1 read-before-write
+-speed false
+-reset_mode sync
---
+++ src/chip/DSP/dsp_data_ram.v
@@ -0,0 +1,99 @@
+//Copyright (C)2014-2023 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: IP file
+//Tool Version: V1.9.9 (64-bit)
+//Part Number: GW5A-LV25MG121NC1/I0
+//Device: GW5A-25
+//Device Version: A
+//Created Time: Thu Feb  8 13:40:31 2024
+
+module dsp_data_ram (douta, doutb, clka, ocea, cea, reseta, wrea, clkb, oceb, ceb, resetb, wreb, ada, dina, adb, dinb);
+
+output [15:0] douta;
+output [15:0] doutb;
+input clka;
+input ocea;
+input cea;
+input reseta;
+input wrea;
+input clkb;
+input oceb;
+input ceb;
+input resetb;
+input wreb;
+input [10:0] ada;
+input [15:0] dina;
+input [10:0] adb;
+input [15:0] dinb;
+
+wire [7:0] dpb_inst_0_douta_w;
+wire [7:0] dpb_inst_0_doutb_w;
+wire [7:0] dpb_inst_1_douta_w;
+wire [7:0] dpb_inst_1_doutb_w;
+wire gw_gnd;
+
+assign gw_gnd = 1'b0;
+
+DPB dpb_inst_0 (
+    .DOA({dpb_inst_0_douta_w[7:0],douta[7:0]}),
+    .DOB({dpb_inst_0_doutb_w[7:0],doutb[7:0]}),
+    .CLKA(clka),
+    .OCEA(ocea),
+    .CEA(cea),
+    .RESETA(reseta),
+    .WREA(wrea),
+    .CLKB(clkb),
+    .OCEB(oceb),
+    .CEB(ceb),
+    .RESETB(resetb),
+    .WREB(wreb),
+    .BLKSELA({gw_gnd,gw_gnd,gw_gnd}),
+    .BLKSELB({gw_gnd,gw_gnd,gw_gnd}),
+    .ADA({ada[10:0],gw_gnd,gw_gnd,gw_gnd}),
+    .DIA({gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,dina[7:0]}),
+    .ADB({adb[10:0],gw_gnd,gw_gnd,gw_gnd}),
+    .DIB({gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,dinb[7:0]})
+);
+
+defparam dpb_inst_0.READ_MODE0 = 1'b0;
+defparam dpb_inst_0.READ_MODE1 = 1'b0;
+defparam dpb_inst_0.WRITE_MODE0 = 2'b00;
+defparam dpb_inst_0.WRITE_MODE1 = 2'b10;
+defparam dpb_inst_0.BIT_WIDTH_0 = 8;
+defparam dpb_inst_0.BIT_WIDTH_1 = 8;
+defparam dpb_inst_0.BLK_SEL_0 = 3'b000;
+defparam dpb_inst_0.BLK_SEL_1 = 3'b000;
+defparam dpb_inst_0.RESET_MODE = "SYNC";
+
+DPB dpb_inst_1 (
+    .DOA({dpb_inst_1_douta_w[7:0],douta[15:8]}),
+    .DOB({dpb_inst_1_doutb_w[7:0],doutb[15:8]}),
+    .CLKA(clka),
+    .OCEA(ocea),
+    .CEA(cea),
+    .RESETA(reseta),
+    .WREA(wrea),
+    .CLKB(clkb),
+    .OCEB(oceb),
+    .CEB(ceb),
+    .RESETB(resetb),
+    .WREB(wreb),
+    .BLKSELA({gw_gnd,gw_gnd,gw_gnd}),
+    .BLKSELB({gw_gnd,gw_gnd,gw_gnd}),
+    .ADA({ada[10:0],gw_gnd,gw_gnd,gw_gnd}),
+    .DIA({gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,dina[15:8]}),
+    .ADB({adb[10:0],gw_gnd,gw_gnd,gw_gnd}),
+    .DIB({gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,dinb[15:8]})
+);
+
+defparam dpb_inst_1.READ_MODE0 = 1'b0;
+defparam dpb_inst_1.READ_MODE1 = 1'b0;
+defparam dpb_inst_1.WRITE_MODE0 = 2'b00;
+defparam dpb_inst_1.WRITE_MODE1 = 2'b10;
+defparam dpb_inst_1.BIT_WIDTH_0 = 8;
+defparam dpb_inst_1.BIT_WIDTH_1 = 8;
+defparam dpb_inst_1.BLK_SEL_0 = 3'b000;
+defparam dpb_inst_1.BLK_SEL_1 = 3'b000;
+defparam dpb_inst_1.RESET_MODE = "SYNC";
+
+endmodule //dsp_data_ram
