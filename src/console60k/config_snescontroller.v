3826f33d7e368f17bfdd90fca04dbea2b0eece19
nand2mario <nand2mario@outlook.com>
2025-01-31 11:51:11 +0800

    add tang console support (controller on pmod1)

---
+++ src/console60k/config_snescontroller.v
@@ -0,0 +1,14 @@
+
+`define PRIMER
+`define SDRAM_3CH
+`define CHIP_DSPn
+// `define CHIP_GSU
+`define CONTROLLER_SNES
+
+package configPackage;  
+
+localparam SDRAM_DATA_WIDTH = 16;
+localparam SDRAM_ROW_WIDTH = 13;
+
+endpackage
+
